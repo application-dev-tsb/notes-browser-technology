2dd64d636530294918cc6ab587cc0ecc184ee130
LM Bibera <secret.transaction@gmail.com>
2017-07-08 16:06:44 +0800

    udemy: aplying css classes dynamically with ngClass

--- Angular/angular-udemy-basics/src/app/server/server.component.ts
+++ Angular/angular-udemy-basics/src/app/server/server.component.ts
@@ -10,9 +10,15 @@ export class ServerComponent implements OnInit {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
-  constructor() { }
+  constructor() { 
+    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
+  }
 
   ngOnInit() {
+  }
+
+  getColor(): string {
+    return this.serverStatus === 'online' ? 'white' : 'pink';
   }
 
 }
