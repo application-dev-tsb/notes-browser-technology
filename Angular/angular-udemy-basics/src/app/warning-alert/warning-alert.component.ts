4094566b59f045d7ffa0c91012c4cd8ac1d90c39
LM Bibera <secret.transaction@gmail.com>
2017-07-08 10:56:08 +0800

    udemy: inlined warning alert

--- Angular/angular-udemy-basics/src/app/warning-alert/warning-alert.component.ts
+++ Angular/angular-udemy-basics/src/app/warning-alert/warning-alert.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  template: '<div class="alert alert-warning" role="alert">{{text}}</div>',
+  styles: ['']
 })
 export class WarningAlertComponent implements OnInit {
 
