428b6e74b9ea7f05a1f509f164225d2e3b037ed5
LM Bibera <secret.transaction@gmail.com>
2017-07-08 20:01:45 +0800

    udemy: added content to the recipe list

--- Angular/angular-udemy-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/angular-udemy-course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../models/recipe';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Awesome Spags', 'Test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfENuLozkxmuUhy755QF6dTnK_yrdqc7IPVcP1LX1tsZB52mpa'),
+    new Recipe('Clam Chowder', 'Delicioso', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--488691_11.jpg?itok=ExaTspz1')
+  ];
 
   constructor() { }
 
