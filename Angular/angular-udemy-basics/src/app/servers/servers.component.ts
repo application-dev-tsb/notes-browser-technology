1f0c47fd853122ce5664998a6c237f03f8e4d835
LM Bibera <secret.transaction@gmail.com>
2017-07-08 15:17:34 +0800

    udemy: added event binding, string interpolation, and tw-way binding examples

--- Angular/angular-udemy-basics/src/app/servers/servers.component.ts
+++ Angular/angular-udemy-basics/src/app/servers/servers.component.ts
@@ -2,18 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <hr/>
-  	<app-server></app-server>
-  	<app-server></app-server>
-  `,
+  templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server Created';
   }
 
 }
