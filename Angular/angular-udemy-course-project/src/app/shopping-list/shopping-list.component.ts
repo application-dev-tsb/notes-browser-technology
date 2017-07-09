b9ff2098c8cc1b8da73b97574c1df4bae7380e7d
LM Bibera <secret.transaction@gmail.com>
2017-07-09 10:20:13 +0800

    udemy: added more gui

--- Angular/angular-udemy-course-project/src/app/shopping-list/shopping-list.component.ts
+++ Angular/angular-udemy-course-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../models/ingredient';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Pepper', 12),
+    new Ingredient('Salt', 1)
+  ];
 
   constructor() { }
 
